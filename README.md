395430674f68a9947cc681954eeacb0f25241c14
Dominik Drexler <drexlerd93@gmail.com>
2025-11-17 12:02:56 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tyr
+
+Tyr aims to become a weighted, annotated, and parallelizable datalog solver based k-clique enumeration in k-partite graph.
---
+++ include/tyr/declarations.hpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2023 Dominik Drexler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MIMIR_DATALOG_DECLARATIONS_HPP_
+#define MIMIR_DATALOG_DECLARATIONS_HPP_
+
+#include <concepts>
+#include <cstddef>
+#include <ranges>
+#include <string>
+#include <string_view>
+
+namespace mimir::datalog
+{
+
+template<typename T>
+concept IsNumber = std::integral<std::remove_cvref_t<T>>;
+
+template<typename T>
+concept IsNumberRandomAccessRange = std::ranges::random_access_range<T> && IsNumber<std::ranges::range_reference_t<T>>;
+
+template<typename T>
+concept IsName = std::convertible_to<T, std::string_view>;
+
+/// We will create a symbol table for variables
+template<typename T>
+concept IsVariable = requires(const T& a) {
+    { a.get_index() } -> IsNumber;
+    { a.get_name() } -> IsName;
+};
+
+template<typename T>
+concept IsRelation = requires(const T& a) {
+    { a.get_index() } -> IsNumber;
+    { a.get_arity() } -> IsNumber;
+};
+
+template<typename T>
+concept IsAtom = requires(const T& a) {
+    { a.get_index() } -> IsNumber;
+    { a.get_relation() } -> IsRelation;
+    { a.get_arguments() } -> IsNumberRandomAccessRange;  /// positive numbers for constants, negative for variables
+};
+
+template<typename T>
+concept IsAtomForwardRange = std::ranges::forward_range<T> && IsAtom<std::ranges::range_reference_t<T>>;
+
+template<typename T>
+concept IsFact = requires(const T& a) {
+    { a.get_index() } -> IsNumber;
+    { a.get_relation() } -> IsRelation;
+    { a.get_arguments() } -> IsNumberRandomAccessRange;
+};
+
+template<typename T>
+concept IsRule = requires(const T& a) {
+    { a.get_index() } -> IsNumber;
+    { a.get_head() } -> IsAtom;
+    { a.get_static_body() } -> IsAtomForwardRange;
+    { a.get_fluent_body() } -> IsAtomForwardRange;
+};
+
+}
+
+#endif
