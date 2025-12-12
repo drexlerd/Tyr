edf9331a732f0b1aa86bd76e6172b5db40ce55fd
Dominik Drexler <drexlerd93@gmail.com>
2025-12-12 17:24:51 +0100

    fix script memory

--- experiments/2025-11-10-ground_task-2.py
+++ experiments/2025-11-10-ground_task-2.py
@@ -47,7 +47,7 @@ if REMOTE:
     ENV = TetralithEnvironment(
         setup=TetralithEnvironment.DEFAULT_SETUP,
         memory_per_cpu="2840M",
-        cpus_per_task=NUM_THREADS,
+        cpus_per_task=3,  # 3*2840 >= 8000
         extra_options="#SBATCH --account=naiss2025-22-1245")
     
 else:
